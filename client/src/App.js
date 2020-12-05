8338a62193eb06f8b53d20b339bd3d402da8ac8c
apassan23 <abhishekpassan99@gmail.com>
2020-12-05 19:13:33 +0530

    added showcase component

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Featured from './components/Featured';
 import AppNavbar from './components/AppNavbar';
 import Carousel from './components/Carousel';
+import Showcase from './components/Showcase';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App w-100'>
       <AppNavbar />
       <Carousel />
       <Featured />
+      <Showcase />
     </div>
   );
 }
