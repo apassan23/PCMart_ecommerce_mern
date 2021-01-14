c9db7df7089f06ccd5f169c33456c53293d9b0bd
apassan23 <abhishekpassan99@gmail.com>
2021-01-14 16:31:07 +0530

    authentication reducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,57 @@
+import {
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  USER_LOADED,
+  USER_LOADING,
+} from '../actions/types';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: false,
+  isLoading: false,
+  user: null,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+      };
+
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload,
+      };
+
+    case LOGIN_FAIL:
+    case LOGOUT_SUCCESS:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+
+    default:
+      return state;
+  }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import productReducer from './productReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
   product: productReducer,
+  auth: authReducer,
 });
