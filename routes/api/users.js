4f9f33e46d57444139d01542896db5163350c08e
apassan23 <abhishekpassan99@gmail.com>
2021-05-08 23:23:42 +0530

    fixed a signup bug

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,30 +11,30 @@ router.post('/', (req, res) => {
     return res.status(400).json({ message: 'All Fields must be filled' });
   else {
     User.findOne({ email }).then((user) => {
-      if (user) return res.status(400).json({ message: 'User Already exists' });
-    });
-
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, (err, hash) => {
-        if (err) throw err;
-        const newUser = new User({ email, username, password: hash });
-        newUser.save().then((user) => {
-          jwt.sign(
-            { id: user._id },
-            config.get('jwtSecret'),
-            { expiresIn: 3600 },
-            (err, token) => {
-              if (err) throw err;
-              res.status(200).json({
-                token,
-                user: {
-                  id: user._id,
-                  email: user.email,
-                  username: user.username,
-                },
-              });
-            }
-          );
+      if (user)
+        return res.status(400).json({ message: 'User Already exists!' });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+          if (err) throw err;
+          const newUser = new User({ email, username, password: hash });
+          newUser.save().then((user) => {
+            jwt.sign(
+              { id: user._id },
+              config.get('jwtSecret'),
+              { expiresIn: 3600 },
+              (err, token) => {
+                if (err) throw err;
+                res.status(200).json({
+                  token,
+                  user: {
+                    id: user._id,
+                    email: user.email,
+                    username: user.username,
+                  },
+                });
+              }
+            );
+          });
         });
       });
     });
