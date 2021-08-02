80650b07db041ee413f9df1e8cd5ebacb59bdef6
apassan23 <abhishekpassan99@gmail.com>
2021-08-03 00:38:43 +0530

    changed some deployment settings

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(config.get('mongoURI'), {
+  .connect(process.env.MONGO_URI || config.get('mongoURI'), {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
